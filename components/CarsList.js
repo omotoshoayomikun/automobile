378cee0306ac6c68322ee649138f9be0470406c7
Ayomikun <omotoshoayomikun@gmail.com>
2022-05-18 13:35:45 +0100

    initial

--- components/CarsList.js
+++ components/CarsList.js
@@ -3,6 +3,10 @@ import Image from 'next/image';
 import { FaShoppingCart } from 'react-icons/fa';
 import { FaInfoCircle } from 'react-icons/fa';
 import Cars from './Cars';
+import { useState } from 'react';
+import Link from 'next/link';
+
+import { FaImages } from 'react-icons/fa';
 
 
 const carList = [
@@ -129,6 +133,8 @@ const carList = [
 ]
 
 const CarList = () => {
+
+    const [view, setView] = useState(false)
     return (
         <div className={styles.container}>
             {
@@ -137,7 +143,37 @@ const CarList = () => {
                         <h1 className={styles.heading}>{carsItem.category}</h1>
                         <div className={styles.wrapper}>
                             {carsItem.cars.map((car, i) => (
-                                <Cars car={car} i={i} />
+                                <div className={styles.car__card} key={i}>
+                                    <div className={styles.img_details_cont} onMouseOver={() => setView(true)} onMouseLeave={() => setView(false)}>
+                                        {view && (
+                                            <div className={styles.img_details}>
+
+                                                <div className={styles.details_wrap}>
+                                                    <div className={styles.detail}>
+                                                        <div>
+                                                            <i className={styles.detail_icon}><FaImages /></i>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )}
+                                    </div>
+                                    <div className={styles.img__wrapper}>
+                                        <Image src={car.img} alt='image' layout='fill' objectFit='cover' className={styles.img} />
+                                    </div>
+                                    <div className={styles.title}>
+                                        <div className={styles.text}>{car.text}</div>
+                                        <div className={styles.amount}>${car.amount}</div>
+                                        <div className={styles.button_cart}>
+                                            <Link href='/car/1' passHref>
+                                                <button className={styles.button}>
+                                                    SHOW DETAILS
+                                                </button>
+                                            </Link>
+                                            <button className={styles.button}> <i className={styles.icons}><FaShoppingCart /> </i>  BUY</button>
+                                        </div>
+                                    </div>
+                                </div>
                             ))}
                         </div>
                     </div>
